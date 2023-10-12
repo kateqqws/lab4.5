7774af3049624cc317c11f35f49a7ee4547288d3
unknown <katepob160@gmail.com>
2023-10-12 22:50:48 +0300

    upload files

---
+++ Lab4.5/Lab4.5.cpp
@@ -0,0 +1,24 @@
+﻿#include <iostream>
+#include <iomanip>
+#include <time.h>
+using namespace std;
+int main()
+{
+	double x, y;
+	srand((unsigned)time(NULL));
+
+	for (int i = 0; i < 10; i++)
+	{
+		x = 6. * rand() / RAND_MAX - 1;
+		y = 8. * rand() / RAND_MAX - 3;
+
+		if ((y >= ((x - 2) * (x - 2) - 3) && (y <= x) && (y >= 0)) ||
+			(y >= ((x - 2) * (x - 2) - 3) && (y <= -x)))
+			cout << setw(8) << setprecision(4) << x << " "
+			<< setw(8) << setprecision(4) << y << " " << "yes" << endl;
+		else
+			cout << setw(8) << setprecision(4) << x << " "
+			<< setw(8) << setprecision(4) << y << " " << "no" << endl;
+	}
+	return 0;
+}
